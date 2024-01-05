9af4f318973f70a4bda55f067dda48bd133752b6
getinthere <getinthere@naver.com>
2024-01-05 11:52:33 +0900

    객체복사, 컬렉션복사

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,18 @@
+// 1. 리턴 타입이 void이 경우
+// 2. 매개변수가 있는 경우
+// 3. 리턴 타입이 있는 경우
+interface Able<T, R> {
+    R hello(T value);
+}
+
+
 public class Hello {
+
+    static void start(Able able) {
+        able.hello(1);
+    }
+
     public static void main(String[] args) {
-        System.out.println("1");
-        System.out.println("4");
-        System.out.println("5");
+        start(value -> 10);
     }
 }
---
+++ src/ex14/example1/CopyEx01.java
@@ -0,0 +1,21 @@
+package ex14.example1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CopyEx01 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        // 컬렉션 복사
+        List<Integer> newList = new ArrayList<>(list);
+        newList.add(5);
+
+        System.out.println(list.size());
+        System.out.println(newList.size());
+    }
+}
